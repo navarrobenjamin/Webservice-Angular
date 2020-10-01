fa0089e15fb1f42ac4ed61a48d21673d57827615
Benjamin <navarrobejamin@gmail.com>
2020-10-01 15:30:23 -0300

    crud

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddComponent } from './Persona/add/add.component';
+import { EditComponent } from './Persona/edit/edit.component';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { ServiceService } from './Service/service.service';
+
+import{HttpClientModule}from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddComponent,
+    EditComponent,
+    ListarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [ServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
